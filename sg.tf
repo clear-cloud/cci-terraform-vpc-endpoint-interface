c748b576711ce515b3a4d958bd3da63422995194
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-20 16:14:21 +0000

    Added required SG

---
+++ sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "endpoint" {
+  name        = "SG for access to Interface type VPC Endpoints in ${var.environment}"
+  description = "Allows access to Interface type VPC Endpoints from ${var.vpc_cidr}"
+  vpc_id      = "${id}"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+ tags {
+    environment   = "${var.environment}"
+    orchestration = "${var.orchestration}"
+  }
+}
