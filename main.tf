a01613f0a1954e7321d6e2d154b4794b81794559
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-20 15:34:30 +0000

    init

---
+++ main.tf
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------
+# VPC Endpoint(s) - INTERFACE type only
+#--------------------------------------------------------------
+
+resource "aws_vpc_endpoint" "endpoint" {
+  count             = "${length(split(",", var.service_name))}"
+  vpc_id            = "${var.vpc_id}"
+  service_name      = "${element(split(",", var.service_name), count.index)}"
+  vpc_endpoint_type = "${var.vpc_endpoint_type}"
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "service_name" {}
+variable "vpc_id" {}
+
+variable "vpc_endpoint_type" {
+  default = "Interface"
+}
