4e1b33a96922acd308834fc9f989227e1a9d7c71
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-20 15:57:09 +0000

    depends on

--- endpoint_subnet_association.tf
+++ endpoint_subnet_association.tf
@@ -1,4 +1,6 @@
 resource "aws_vpc_endpoint_subnet_association" "association" {
+ depend_on = ["aws_vpc_endpoint.endpoint"]
+
   count           = "${length(split(",", var.subnet_id))}"
   vpc_endpoint_id = "${aws_vpc_endpoint.endpoint.id}"
   subnet_id       = "${element(split(",", var.subnet_id), count.index)}"
