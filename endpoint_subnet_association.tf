25813544572ffab838c15749aabaf6f0e355f7a5
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-20 16:34:29 +0000

    subnet associations

--- endpoint_subnet_association.tf
+++ endpoint_subnet_association.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_endpoint_subnet_association" "association" {
- depends_on = ["aws_vpc_endpoint.endpoint"]
+  depends_on = ["aws_vpc_endpoint.endpoint"]
 
-  count           = "${length(split(",", var.subnet_id))}"
+  count           = "${length(split(",", var.service_name))}"
   vpc_endpoint_id = "${aws_vpc_endpoint.endpoint.id}"
   subnet_id       = "${element(split(",", var.subnet_id), count.index)}"
 }
